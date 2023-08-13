6474bc76a4f51ca581e839ff1da2288cd6fb9a31
pavanb0 <pavanbagwe16@gmail.com>
2023-08-13 23:07:01 +0530

    kth frequent

---
+++ ArrayHashing/Kthfrequent.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq_dict = defaultdict(int)
+        result = []
+        freq_list = []
+
+        for num in nums:
+            if num in freq_dict:
+                freq_dict[num] += 1
+            else:
+                freq_dict[num] = 1
+
+        for freq in freq_dict.values():
+            freq_list.append(freq)
+
+        freq_list.sort()
+        freq_list = freq_list[-k:]
+
+        for freq in freq_list[:k]:
+            for num in freq_dict:
+                if freq_dict[num] == freq and num not in result:
+                    result.append(num)
+
+        return result
