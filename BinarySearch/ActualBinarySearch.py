d0eaac747ea4a069212737b0de569cac2168f831
pavanb0 <pavanbagwe16@gmail.com>
2023-08-20 22:48:53 +0530

    Binary Search

---
+++ BinarySearch/ActualBinarySearch.py
@@ -0,0 +1,10 @@
+
+left,right = 0,len(nums)-1
+while left<=right:
+    mid = (right+left) // 2
+    if nums[mid] == target:
+        print(mid)
+    elif nums[mid]<target:
+        left = mid +1
+    else:
+        right = mid -1
