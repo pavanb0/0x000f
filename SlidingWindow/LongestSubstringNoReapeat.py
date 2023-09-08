0e09c06b59a5076595c2acabca83fee20d445464
pavanb0 <pavanbagwe16@gmail.com>
2023-09-08 23:11:13 +0530

    Sliding window new problem solved

---
+++ SlidingWindow/LongestSubstringNoReapeat.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        ar = set()
+        out = 0
+        l = 0
+        m = 0
+        while l < len(s):
+            if s[l] not in ar:
+                ar.add(s[l])
+                l+=1
+                out = max(len(ar),out)
+            else:
+                ar.remove(s[m])
+                m+=1
+        return out
