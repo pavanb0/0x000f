8095ebbd84331ba417bcdf997835ed1a2faef265
pavan bagwe <96296445+pavanb0@users.noreply.github.com>
2023-09-18 19:47:25 +0530

    Create countAsterisks.py

---
+++ countAsterisks.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        c=0
+        flag=False
+        for i in s:
+            if (i=='|'):
+                flag=not(flag)
+            if(i=='*' and flag==False):
+                c=c+1
+        return c
